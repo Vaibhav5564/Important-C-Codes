5788a2ca9050d4d01449d8ce00f02350150d3627
Vaibhav Rajendra Adsul <vaibhavadsul5564@gmail.com>
2026-02-18 09:00:57 +0000

    Static-And-Dynamic-Implementation -of-Topological-Sort

--- Graphs/topologicalSort.c
+++ Graphs/topologicalSort.c
@@ -1,3 +1,67 @@
+
+// #include <stdio.h>
+
+// int visited[10], stack[10], top = -1;
+
+// void push(int data){
+//     stack[++top] = data;
+// }
+
+// int pop(){
+//     return stack[top--];
+// }
+
+// void topologicalSort(int adj[][10], int curr, int n){
+//     int i;
+//     visited[curr] = 1;
+//     for(i=0; i<n; i++){
+//         if(adj[curr][i] == 1 && visited[i] == 0)
+//         {
+//             topologicalSort(adj, i, n);
+//         }
+//     }
+//     push(curr);
+// }
+// int main() {
+    
+//     int n, i, j, u, v, edges, adj[10][10];
+    
+//     printf("Enter How Many Vertices is in Graph - ");
+//     scanf("%d",&n);
+    
+//     for(i=0; i<n; i++){
+//         for(j=0; j<n; j++){
+//             adj[i][j] = 0;
+//         }
+//     }
+    
+//     printf("Enter No. of Edges in a Graph - ");
+//     scanf("%d",&edges);
+    
+//     printf("Enter Edges as (u v):\nwhere\nu--->v\n\n");
+    
+//     for(i=0; i<edges; i++){
+//         printf("Enter edge as (U V): - ");
+//         scanf("%d%d", &u, &v);
+//         adj[u][v] = 1;
+//     }
+    
+//     printf("\n\nTopological Sort -");
+    
+//     for(i=0; i<n; i++){
+//      if(visited[i] == 0){
+//         topologicalSort(adj, i, n);
+//     }
+// }
+    
+//     while(top >= 0){
+//         printf("\t%d",stack[top]);
+//         top--;
+//     }
+  
+//     return 0;
+// }
+
 #include <stdlib.h>
 #include <stdio.h>
 
